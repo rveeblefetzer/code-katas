1086e77a39f63234601e698eb1e7d59f4c279773
Rick Valenzuela <rv@rickv.com>
2016-12-12 01:08:18 -0800

    Update vowel-count.py
    Add docstring

--- vowel-count.py
+++ vowel-count.py
@@ -1,10 +1,7 @@
 def getCount(inputStr):
+    """Count the number of lowercase vowels in a string."""
     num_vowels = 0
-    # your code here
-
     vowels = ['a', 'e', 'i', 'o', 'u']
-
     for c in list(inputStr):
         if c in vowels: num_vowels+=1
-
     return num_vowels
