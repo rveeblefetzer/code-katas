133566766758b1214a344a982ea168cb61ff55cd
Rick Valenzuela <rv@rickv.com>
2016-12-12 01:05:46 -0800

    Update ones-and-zeros.py

--- ones-and-zeros.py
+++ ones-and-zeros.py
@@ -1,4 +1,5 @@
 def binary_array_to_number(arr):
+  """Given list of 1s and 0s, convert equivalent binary value to an integer."""
   in_string = []
   for num in arr:
      in_string.append(str(num))
