a745ec88603d6aae388c61aff367b93682c12495
Rick Valenzuela <devnull@rickv.com>
2016-12-18 23:07:18 -0800

    adding test file

---
+++ src/proper_parentheticals.py
@@ -0,0 +1,109 @@
+"""A linked list to verify parenthetical pairs."""
+
+
+class Node(object):
+    """Create class to represent individual node elements in data structure."""
+
+    def __init__(self, data, next_node=None):
+        """Something."""
+        self.data = data
+        self.next_node = next_node
+
+
+class LinkedList(object):
+    """Create linked lists."""
+
+    def __init__(self, iterable=None):
+        """Instantiate linked-list object with optional iterable."""
+        self.head = None
+        if iterable and hasattr(iterable, "__iter__"):
+            for value in iterable:
+                self.push(value)
+        # for py27
+        elif iterable and isinstance(iterable, str):
+            for char in iterable:
+                self.push(char)
+        elif iterable:
+            raise TypeError
+
+    def push(self, val):
+        """Push elements into list."""
+        node = Node(val)
+        node.next_node = self.head
+        self.head = node
+
+    def pop(self):
+        """Remove head element and return value."""
+        temp_data = self.head.data
+        self.head = self.head.next_node
+        return temp_data
+
+    def size(self):
+        """Return length of list."""
+        count = 0
+        current = self.head
+        if self.head is None:
+            return 0
+        while current.next_node is not None:
+            count += 1
+            current = current.next_node
+        return count + 1
+
+    def search(self, val):
+        """Return node with data value of val."""
+        current = self.head
+        # import pdb; pdb.set_trace()
+        while current is not None:
+            if current.data == val:
+                return current
+            current = current.next_node
+        return None
+
+    def remove(self, node):
+        """Remove given node from list."""
+        current = self.head
+        if current.data == node.data:
+            self.head = current.next_node
+            return True
+        while current.next_node is not None:
+            if current.next.data == node.data:
+                current.next_node = current.next_node.next_node
+                return True
+            current = current.next_node
+        raise ValueError("Node not in list.")
+
+    def display(self):
+        """Display all elements in a linked list."""
+        current = self.head
+        result = "("
+        while current is not None:
+            if isinstance(current.data, str):
+                result = result + "'" + current.data + "'"
+            else:
+                result = result + str(current.data)
+            if current.next_node is not None:
+                result += ", "
+            current = current.next_node
+        result += ')'
+        print(result)
+        return result
+
+
+def parentheses_check(string):
+    """Check if parentheticals close."""
+    revstring = string[::-1]
+    string = LinkedList(revstring)
+    state = 0
+    current = string.head
+    while current is not None:
+        if current.data == '(':
+            state += 1
+        else:
+            state -= 1
+        if state < 0:
+            return -1
+        current = current.next_node
+    print('this is the state:', state)
+    if state > 0:
+        return 1
+    return 0
---
+++ src/test_parenthetics.py
@@ -0,0 +1,23 @@
+"""Tests for proper_parenthetics."""
+
+import pytest
+
+PARENS_TABLE = [
+    [")))(((", -1],
+    ['instantfail)', -1],
+    ['(evens out)(but order bad))('"', -1],)
+    [";; straight from my .emacs file (add-hook 'org-mode-hook
+        ;;  (lambda ()
+            ;;  (add-hook 'before-save-hook 'my/org-add-ids-to-headlines-in-file
+                nil 'local)))", 0],
+    ['(((one not closed))', 1],
+    ['()', 0],
+    ['(()())', 0]
+]
+
+
+@pytest.mark.parametrize("string, result", PARENS_TABLE)
+def test_parens_ispaired(string, result):
+    """Test if string of parentheses all close."""
+    from proper_parenthetics import parentheses_check
+    assert parentheses_check(string) == result
